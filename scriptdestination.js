5fdd9f797fdd4ea49eb8eb554312be4446355515
Mimi-sang <minatasangare967@gmail.com>
2024-10-21 12:37:03 +0000

    #

---
+++ scriptdestination.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    // Logique pour gérer le formulaire, par exemple, validation
+    console.log('Formulaire soumis');
+});
