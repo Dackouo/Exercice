89075ff73c60d7858d0e97c6311796f18ae876a9
Dackouo <mimicharonneamina@gmail.com>
2024-10-21 22:37:02 +0000

    update

--- inscription.php
+++ inscription.php
@@ -3,8 +3,8 @@ include('database.php');
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $motdepasse = $_POST['mdp'];
-$confirmPassword = $_POST['mdpconf'];
-$requete = $bdb->exec("INSERT into inscription(nom, prenom, motdepasse, confirmPassword) values ('$nom','$prenom','$mdp', '$mdpconf')");
+$confirmMotdepasse = $_POST['mdpconf'];
+$requete = $bdb->execute_query("INSERT into inscription(nom, prenom, motdepasse, confirmPassword) values ('$nom','$prenom','$mdp', '$mdpconf')");
 
 if ($requete == true) {
     echo "insertion effectuée avec succès";
